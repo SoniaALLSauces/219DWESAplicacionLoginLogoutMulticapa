adbd50c8fb5d763dc4e517bddb4373eee53c2390
Sonia <Sonia>
2022-01-14 09:30:02 +0100

    day 14/01/22 - inhabilito detalles para Explotacion (da errores)

--- controller/cInicioPrivado.php
+++ controller/cInicioPrivado.php
@@ -15,11 +15,11 @@
                 $conexionAnterior = $usuarioActual->getFechaHoraConexionAnterior(); //recuperamos la fecha de la conexion anterior del usuario
         
     //Si pulso en detalle:
-        if (isset($_REQUEST['detalle'])){
-            $_SESSION['pagina']='detalle';  //cuando existe una sesion, abro el controlador que hay en la variable
-            header('Location: index.php');  //recargo el fichero index.php
-                exit;
-        }
+//        if (isset($_REQUEST['detalle'])){
+//            $_SESSION['pagina']='detalle';  //cuando existe una sesion, abro el controlador que hay en la variable
+//            header('Location: index.php');  //recargo el fichero index.php
+//                exit;
+//        }
             
 
     //salida:
