ad4c4d3895ad3727e4c4ef10578de9d4ec255327
Sonia <Sonia>
2022-01-20 13:27:42 +0100

    day 20/01/2022 - corregir cambios para eliminar error

--- config/confDB.php
+++ config/confDB.php
@@ -6,7 +6,7 @@
  * Variables para establecer la conexion PDO con la base de datos en Entorno Desarrollo 192.168.3.119
  */
 
-    /* Constantes BD Entorno Desarrollo One&One */
+    /* Constantes BD Entorno Desarrollo Local */
 //        define ('HOST', 'mysql:host=192.168.3.119;dbname=DAW219DBDepartamentos');
 //        define ('USER', 'usuarioDAW219DBDepartamentos');
 //        define ('PASSWORD', 'P@ssw0rd');
@@ -17,8 +17,9 @@
         define ('PASSWORD', 'paso');
         
         
-    /* Constantes BD Entorno Desarrollo Casa */
-//        define ('HOST', 'mysql:host=192.168.1.119;dbname=DB219DWESLoginLogoutMulticapa');
-//        define ('USER', 'user219DWESLoginLogoutMulticapa');
+    /* Constantes BD Entorno Desarrollo mi Casa */
+//        define ('HOST', 'mysql:host=192.168.1.119;dbname=DAW219DBDepartamentos');
+//        define ('USER', 'usuarioDAW219DBDepartamentos');
 //        define ('PASSWORD', 'P@ssw0rd');
+
 
