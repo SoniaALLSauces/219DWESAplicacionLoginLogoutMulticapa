aba93ed05217b155a8aafa20101236f6302d4fda
Sonia <Sonia>
2022-01-20 13:47:27 +0100

    day 20/01/2022 - activada configuracion para internet. Activada pagina publica, privada tras login y logout

--- config/confDB.php
+++ config/confDB.php
@@ -12,8 +12,8 @@
 //        define ('PASSWORD', 'P@ssw0rd');
         
     /* Constantes BD Entorno Desarrollo Local */
-//        define ('HOST', 'mysql:host=daw219.sauces.local;dbname=DB219DWESProyectoTema5');
-//        define ('USER', 'user219DWESProyectoTema5');
+//        define ('HOST', 'mysql:host=daw219.sauces.local;dbname=DB219DWESLoginLogoutTema5');
+//        define ('USER', 'user219DWESLoginLogoutTema5');
 //        define ('PASSWORD', 'paso');
         
         /* Constantes BD Entorno Desarrollo ONE&ONE */
@@ -26,4 +26,5 @@
 //        define ('HOST', 'mysql:host=192.168.1.119;dbname=DAW219DBDepartamentos');
 //        define ('USER', 'usuarioDAW219DBDepartamentos');
 //        define ('PASSWORD', 'P@ssw0rd');
+
 
